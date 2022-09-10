a7c979277e5c0f102dea08be0e8ee16a7de14efc
MartinSolera <martinsolera71@gmail.com>
2022-09-10 12:57:48 -0300

    Se creo insertar ciudad

--- src/main/java/datos/CiudadDAOImplementss.java
+++ src/main/java/datos/CiudadDAOImplementss.java
@@ -8,10 +8,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CiudadDAOImplementss implements ICiudadDAO {
 
+    Scanner sc = new Scanner(System.in);
+
     private static final String SQL_SELECT = "SELECT * FROM ciudades.ciudad;";
+    private static final String SQL_INSERT = "INSERT INTO ciudades.ciudad(nombre,pais, anio_visitada, veces_visitada, compas) VALUES (?,?,?,?,?)";
 
     @Override
     public List<CiudadDTO> verCiudades() {
@@ -49,4 +53,69 @@ public class CiudadDAOImplementss implements ICiudadDAO {
         }
         return listaCiudades;
     }
+
+    public void insertarCiudad(){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int registro = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+
+            CiudadDTO ciudadNueva = crearCiudad();
+
+            stmt.setString(1,ciudadNueva.getNombre());
+            stmt.setString(2,ciudadNueva.getPais());
+            stmt.setInt(3, ciudadNueva.getAnio_visitada());
+            stmt.setInt(4,ciudadNueva.getVeces_visitada());
+            stmt.setString(5, ciudadNueva.getCompas());
+
+            registro = stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String solicitarValorString(){
+        System.out.println("Ingrese el valor: ");
+        String valor = sc.next();
+        return valor;
+    }
+
+    public CiudadDTO crearCiudad(){
+        CiudadDTO ciudad = new CiudadDTO();
+
+        System.out.println("Nombre Ciudad:");
+        ciudad.setNombre(sc.next());
+
+        System.out.println("Nombre Pais:");
+        ciudad.setPais(sc.next());
+
+        System.out.println("Año visitada:");
+        ciudad.setAnio_visitada(sc.nextInt());
+
+        System.out.println("Cantidad de veces visitada:");
+        ciudad.setVeces_visitada(sc.nextInt());
+
+        System.out.println("Compañeros de viaje:");
+        ciudad.setCompas(sc.next());
+
+        return ciudad;
+    }
+
+
+
+
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,23 +8,35 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         CiudadDAOImplementss ciudadDAO = new CiudadDAOImplementss();
+
+        ///AGREGAR CIUDAD
+        ciudadDAO.insertarCiudad();
+
+        ///VER LISTA CIUDADAES
         List<CiudadDTO> listaCiudades = ciudadDAO.verCiudades();
-        mostarPersona(listaCiudades);
+        mostarCiudades(listaCiudades);
+
+
     }
 
-    public static void mostarPersona(List<CiudadDTO> listaCiudades){
+    public static void mostarCiudades(List<CiudadDTO> listaCiudades){
         int i = 0;
         for(CiudadDTO ciudad : listaCiudades){
             i++;
             System.out.println("Ciudad Nro: " + i);
-            System.out.println("ID: " + ciudad.getId_ciudad());
-            System.out.println("Nombre Ciudad: " + ciudad.getNombre());
-            System.out.println("Pais: " + ciudad.getPais());
-            System.out.println("Año Visitada: " + ciudad.getAnio_visitada());
-            System.out.println("Año Visitada: " + ciudad.getVeces_visitada());
-            System.out.println("La visitaste con: " + ciudad.getCompas());
-            System.out.println("----------------");
+            mostrarCiudad(ciudad);
         }
     }
+
+    public static void mostrarCiudad(CiudadDTO ciudad){
+        System.out.println("ID: " + ciudad.getId_ciudad());
+        System.out.println("Nombre Ciudad: " + ciudad.getNombre());
+        System.out.println("Pais: " + ciudad.getPais());
+        System.out.println("Año Visitada: " + ciudad.getAnio_visitada());
+        System.out.println("Año Visitada: " + ciudad.getVeces_visitada());
+        System.out.println("La visitaste con: " + ciudad.getCompas());
+        System.out.println("----------------");
+    }
+
 
 }
